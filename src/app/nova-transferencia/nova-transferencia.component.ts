3d661939e45fa5a77e9b1385bc5954a80e201a29
Beck Raika <beckraika@gmail.com>
2022-01-05 16:00:28 -0300

    init

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitado nova transferencia');
+    const valorEmitir = { valor: this.valor, destino: this.destino };
+    this.aoTransferir.emit(valorEmitir);
+  }
+}
